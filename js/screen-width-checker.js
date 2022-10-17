3f8858b9234edf21fab9855de98c0c3acab10d33
ggcldev <ggcldev@gmail.com>
2022-10-17 19:11:06 +0800

    Remove media query checker

--- js/screen-width-checker.js
+++
@@ -1,8 +0,0 @@
-function showBrowserWidth() {
-	const width = window.innerWidth
-
-	document.querySelector('h2').innerHTML = `Width: ${width}`
-}
-
-window.onload = showBrowserWidth
-window.onresize = showBrowserWidth
